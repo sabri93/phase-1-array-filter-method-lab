57b27ae917815192386a61c1c7bf854dca21aad7
sabrina <sabrinadjebbar93@gmail.com>
2022-03-31 19:33:07 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Code your solution here
+
+function findMatching(arr,string){
+      
+    const nameMatching = arr.filter(function(driver){
+         if(string ===  driver){
+             return true
+
+         }
+         
+        if (driver.toLowerCase()===string.toLowerCase()){
+            return true 
+        }
+            
+          
+    })
+         
+  
+    return nameMatching; 
+} 
+
+
+
+function fuzzyMatch(arr,str){
+
+      const letterMatching = arr.filter(function(driver){
+      return driver.indexOf(str) === 0          
+      
+   })
+
+ return letterMatching;
+}
+
+
+function matchName(drivers, string){
+
+    const match = drivers.filter(function(driver){
+       return driver.name === string
+
+    })
+  
+    return match;
+
+}
+
+
+
+
